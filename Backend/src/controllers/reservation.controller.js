1a8c77b42bb1b9b9333e5ad35ec7b2d2500247da
Vikash Kumar Chaubey <230101120283@centurionuniv.edu.in>
2026-06-01 23:30:05 +0530

    Update reservation.controller.js

--- Backend/src/controllers/reservation.controller.js
+++ Backend/src/controllers/reservation.controller.js
@@ -1,7 +1,7 @@
 import Reservation from "../models/Reservation.js";
 import Restaurant from "../models/Restaurant.js";
 
-// Create 
+// Create reservation
 export const createReservation = async (req, res, next) => {
   try {
     const { restaurantId, reservationDate, guests } = req.body;
