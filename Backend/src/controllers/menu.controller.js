3e08bb246d88326bd4ed2307e00d64f3b1353eb2
Vikash Kumar Chaubey <230101120283@centurionuniv.edu.in>
2026-06-01 23:28:37 +0530

    Update menu.controller.js

--- Backend/src/controllers/menu.controller.js
+++ Backend/src/controllers/menu.controller.js
@@ -1,7 +1,7 @@
 import MenuItem from "../models/MenuItem.js";
 
 
-// Create menu 
+// Create menu item
 export const createMenuItem = async (req, res, next) => {
   try {
     const { restaurant, name, description, price, image, category } =
