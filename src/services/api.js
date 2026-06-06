e8c093ab00e231483b2b9acdda64ed3d6fb5a6a2
Vikash Kumar Chaubey <230101120283@centurionuniv.edu.in>
2026-06-06 22:15:46 +0530

    Add JWT token interceptor for API requests
    Automatically attach JWT token to request headers.

--- src/services/api.js
+++ src/services/api.js
@@ -7,4 +7,16 @@ const API = axios.create({
   },
 });
 
+// Attach JWT token to every request automatically
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default API;
