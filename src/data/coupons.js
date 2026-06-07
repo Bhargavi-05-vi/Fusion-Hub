4a691f4b2902816884933d7db4fb8e35fc927d2f
DhruvSingh10076 <ds3904431@gmail.com>
2026-06-07 22:21:41 +0530

    Create coupons.js

---
+++ src/data/coupons.js
@@ -0,0 +1,28 @@
+export const coupons = [
+  {
+    code: "WELCOME50",
+    type: "flat",
+    value: 50,
+    minOrder: 299,
+  },
+  {
+    code: "SAVE20",
+    type: "percent",
+    value: 20,
+    minOrder: 499,
+    maxDiscount: 200,
+  },
+  {
+    code: "FOODIE30",
+    type: "percent",
+    value: 30,
+    minOrder: 699,
+    maxDiscount: 300,
+  },
+  {
+    code: "FUSION100",
+    type: "flat",
+    value: 100,
+    minOrder: 799,
+  },
+];
